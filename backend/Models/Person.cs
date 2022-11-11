188bdc07b18beadc0444e868bf45231c7c9f2393
kallnascimento <iksn7@icloud.com>
2022-11-11 16:37:42 -0300

    models implemented

---
+++ backend/Models/Person.cs
@@ -0,0 +1,17 @@
+namespace backend.Models
+{
+    public class Person
+    {
+
+        public Person() { }
+        public Person(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+}
