98d29a435366cb011212d1ebc19082547300c4c5
kallnascimento <iksn7@icloud.com>
2022-11-23 11:32:42 -0300

    todo form and types add

--- frontend/src/app/components/todo-master/todo-master.component.ts
+++ frontend/src/app/components/todo-master/todo-master.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-master',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-master.component.css']
 })
 export class TodoMasterComponent implements OnInit {
-
+  todoForm!: FormGroup
   constructor() { }
 
   ngOnInit(): void {
   }
+  submit() {
+    console.log("Formulario enviado");
 
+  }
 }
